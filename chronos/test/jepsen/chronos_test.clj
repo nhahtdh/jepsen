9202032da2ebeacf4620bece996465c0c6812b32
Hong Dai Thanh <nhahtdh@gmail.com>
2016-05-19 17:26:40 +0700

    Modify chronos to use Ubuntu repository and packages

--- chronos/test/jepsen/chronos_test.clj
+++ chronos/test/jepsen/chronos_test.clj
@@ -4,5 +4,5 @@
             [jepsen.chronos :refer :all]))
 
 (deftest install-test
-  (is (:valid? (:results (jepsen/run! (simple-test "0.23.0-1.0.debian81"
-                                                   "2.3.4-1.0.81.debian77"))))))
+  (is (:valid? (:results (jepsen/run! (simple-test "0.28.1-2.0.20.ubuntu1204"
+                                            "2.4.0-0.1.20151007110204.ubuntu1204"))))))
